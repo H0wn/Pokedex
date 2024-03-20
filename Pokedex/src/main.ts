0b577fbe94b34f8136eb08a01ea3f58d89c64f6f
H0wn <giuseppegenco.27@gmail.com>
2024-03-20 17:19:32 +0100

    feat(): add of PokedexPage and PokemonInfo

--- Pokedex/src/main.ts
+++ Pokedex/src/main.ts
@@ -1,6 +1,14 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+/* import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') */
+import './assets/base.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
